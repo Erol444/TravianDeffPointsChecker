da5b7b89b46092cfa3cc997669b8ca9a8b8a8afc
Erik Kokalj <erik.kokalj@achtzig20.com>
2020-01-07 14:36:47 +0100

    init commit

---
+++ TravianDeffPointsChecker/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Who_sDeffingMyFarms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lab = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1158, 793);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.lab);
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1150, 767);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(91, 63);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 25);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Check again";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "This is 1. check";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(118, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Pages";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 63);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 25);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Check all";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(69, 37);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Check ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "tx3.travian.si";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lab.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lab.Location = new System.Drawing.Point(-2, 0);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(170, 107);
+            this.lab.TabIndex = 11;
+            this.lab.Text = "     ";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1144, 761);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1150, 767);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(370, 35);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(168, 724);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(8, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(203, 724);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(370, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(168, 23);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Update";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 793);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ TravianDeffPointsChecker/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Threading;
+
+namespace Who_sDeffingMyFarms
+{
+    public partial class Form1 : Form
+    {
+
+        public string sql;
+        public SQLiteConnection con;
+        public SQLiteCommand command;
+
+        Boolean webchecked = false;
+        Boolean firstCheck = true;
+
+        public static List<Deffers> DefList = new List<Deffers>();
+        public static List<Deffers> DefList2 = new List<Deffers>();
+        public static List<DeffDiff> DiffList = new List<DeffDiff>();
+        public int PageNum=0;
+        public string serverlink = "tx3.travian.com.hr";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serverlink = textBox1.Text;
+            webBrowser1.Navigate(textBox1.Text+ "/statistiken.php?id=0&idSub=2&page=1");
+        }
+
+        private void button2_Click(object sender, EventArgs e) //CHECK NUM
+        {
+            checking();
+            PageNum = Convert.ToInt16(numericUpDown1.Value);
+            Thread chck = new Thread(new ThreadStart(Check));
+            chck.Start();
+        }
+
+        public void GoTo(int num) {
+            webBrowser1.Navigate(serverlink + "/statistiken.php?id=0&idSub=2&page="+num);
+        }
+
+        public void checking() {
+            //DefList.Clear();
+            button2.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e) //CHECK ALL
+        {
+            checking();
+            HtmlElementCollection b = webBrowser1.Document.GetElementsByTagName("a");
+            int last = 0;
+            bool locked = false;
+            for (int i = 0; i < b.Count; i++)
+            {
+                if (locked)
+                {
+                    last = Convert.ToInt16(b[i].InnerHtml);
+                    break;
+                }
+                try
+                {
+                    if (Convert.ToInt16(b[i].InnerHtml) == 2)
+                    {
+                        locked = true;
+                    }
+                }
+                catch { }
+
+            }
+            PageNum = last;
+            Thread chck = new Thread(new ThreadStart(Check));
+            chck.Start();
+        }
+
+        private void DoInvoke(MethodInvoker del)
+        {
+            if (InvokeRequired) { Invoke(del); }
+            else { del(); }
+        }
+
+        public void Check() {
+            DoInvoke(delegate { CheckPage(); });
+            for (int i = 2; i <= PageNum; i++)
+            {
+                Console.WriteLine("GoTo page " + i);
+                webchecked = false;
+                DoInvoke(delegate { GoTo(i); });
+                Random rnd = new Random();
+                while (!webchecked) {
+                    Thread.Sleep(rnd.Next(30, 100));
+                }
+                Thread.Sleep(rnd.Next(10, 30));
+                DoInvoke(delegate { CheckPage(); });
+                webchecked = false;
+                Thread.Sleep(rnd.Next(10, 30));
+            }
+            try
+            {
+                DoInvoke(delegate { Done(); });
+                database();
+            }
+            catch { Console.WriteLine("error"); }            
+        }
+
+        public void Done() {
+            if (!firstCheck)
+            {
+                try {
+                    foreach (Deffers acc in DefList)
+                {
+                    Deffers deffer = DefList2.First(o => o.name.Equals(acc.name));
+                    if (deffer != null) {
+                        int deffpoints2 = deffer.deffPoints;
+                        if (!(deffpoints2 == acc.deffPoints))
+                        {
+                            DiffList.Add(new DeffDiff
+                            {
+                                name = acc.name,
+                                Diff = deffpoints2 - acc.deffPoints
+                            });
+                        }
+                    }
+                }
+                UpdateDiffTable();
+                } catch {
+                }
+                
+            }
+            else {
+                label2.Text = "Finished";
+            }
+            UpdateBox();
+            button5.Enabled = true;
+            label2.Text = "This is 2.check";
+            firstCheck = false;
+            GoTo(1);
+        }
+
+        public void CheckPage() {
+            HtmlElementCollection b = webBrowser1.Document.GetElementsByTagName("tr");
+
+            for (int i = 1; i < b.Count-1; i++)
+            {
+                HtmlElementCollection TD = b[i].GetElementsByTagName("td");
+                if (!String.IsNullOrEmpty(TD[1].InnerText))
+                {
+                    if (firstCheck)
+                    {
+                        DefList.Add(new Deffers
+                        {
+                            rang = Convert.ToInt16(TD[0].InnerText.Trim('.')),
+                            //name = TD[1].InnerText.Trim(' ').Replace(' ', '_').Replace('.', '_').Replace('-', '_'),
+                            name = TD[1].InnerText,
+                            pop = Convert.ToInt32(TD[2].InnerText),
+                            deffPoints = Convert.ToInt32(TD[4].InnerText)
+                        });
+                        Console.WriteLine("deffer name: "+TD[1].InnerText);
+                    }
+                    else
+                    {
+                        DefList2.Add(new Deffers
+                        {
+                            rang = Convert.ToInt16(TD[0].InnerText.Trim('.')),
+                            //name = TD[1].InnerText.Trim(' ').Replace(' ', '_').Replace('.', '_').Replace('-', '_'),
+                            name = TD[1].InnerText,
+                            pop = Convert.ToInt32(TD[2].InnerText),
+                            deffPoints = Convert.ToInt32(TD[4].InnerText)
+                        });
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UpdateBox();
+        }
+        public void UpdateBox() {
+            richTextBox1.Text = "";
+            foreach (Deffers a in DefList)
+            {
+                richTextBox1.AppendText(a.rang + ". " + a.name + " - " + a.deffPoints + "\n");
+            }
+        }
+        public void database() {
+            con = new SQLiteConnection("Data Source = DB.sqlite; Version = 3");
+            con.Open();
+            foreach (Deffers acc in DefList) {
+                sql = "CREATE TABLE IF NOT EXISTS "+acc.name.Trim(' ').Replace(' ', '_').Replace('.', '_').Replace('-', '_')+" (timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, points NUMERIC, pop NUMERIC, rang NUMERIC)";
+                command = new SQLiteCommand(sql, con);
+                command.ExecuteNonQuery();
+                sql = "insert into "+acc.name+"(points, pop, rang) values('" + acc.deffPoints + "','" + acc.pop + "','" + acc.rang + "')";
+                command = new SQLiteCommand(sql, con);
+                command.ExecuteNonQuery();
+            }
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Thread chck = new Thread(new ThreadStart(Check));
+            chck.Start();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webchecked = true;
+        }
+
+        //----------------------CLASSES-----------------
+        public class Deffers
+        {
+            public string name { get; set; }
+            public int deffPoints { get; set; }
+            public int pop { get; set; }
+            public int rang { get; set; }
+        }
+
+        public class DeffDiff
+        {
+            public string name { get; set; }
+            public int Diff { get; set; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            UpdateDiffTable();
+        }
+
+        private void UpdateDiffTable() {
+            richTextBox2.Text = "";
+            List<DeffDiff> SortedDif = DiffList.OrderBy(o => o.Diff).ToList();
+            foreach (DeffDiff o in SortedDif)
+            {
+                richTextBox2.AppendText(o.name + ": " + o.Diff + "\n");
+            }
+        }
+    }
+
+}
